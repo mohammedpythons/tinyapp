1c522e837520906429ae083fe10ab35ef500f34a
mohammedpythons <mohammedshokai99@gmail.com>
2021-07-05 14:32:10 -0400

    rendring a ejs file

--- express_server.js
+++ express_server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
-
+app.set("view engine", "ejs");
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.set("view engine", "ejs");
+
 
 app.get("/", (req, res) => {
     res.send("Hello");
@@ -20,6 +20,12 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n")
 })
+
+app.get("/urls", (req, res) => {
+    const templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+})
+
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port: ${PORT}`);

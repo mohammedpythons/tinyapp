d4eb8641a3876fbf6e526ff29ca880c9166faa3b
mohammedpythons <mohammedshokai99@gmail.com>
2021-07-06 15:38:26 -0400

    updating long URLs

--- express_server.js
+++ express_server.js
@@ -82,6 +82,15 @@ app.post(`/urls/:shortURL/delete`, (req, res) => {
 });
 
 
+app.post("/urls/:shortURL/update/", (req, res) => {
+    const shortURL = req.params.shortURL;
+    const longURL = req.body.longURL;
+    urlDatabase[shortURL] = longURL;
+  
+    
+    res.redirect(`/urls`); 
+})
+
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port: ${PORT}`);
